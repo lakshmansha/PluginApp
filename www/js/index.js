88aca405288f32e0fd303d8bc64602f6f1ed0810
Lakshman.S <haranhari_38@outlook.com>
2020-05-24 01:01:55 +0530

    Updated Plugin.

--- www/js/index.js
+++ www/js/index.js
@@ -73,7 +73,7 @@ var app = {
                             }
                         } else {
                             message = "Unable to call Error:" + err;
-                        }    
+                        } 
                         
                         AppHelper.alert(message);
                     },
